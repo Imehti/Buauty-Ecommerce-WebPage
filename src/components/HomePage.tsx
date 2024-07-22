83fa2e96c88fa7388969368d656eb387ed6b0a0c
Mahdi <khajavim497@gmail.com>
2024-07-22 17:48:11 +0430

    add background to home page

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+    return (
+       <>
+       <div className="bg-[url('src/assets/bacground.avif')] bg-no-repeat h-full bg-cover">
+
+       </div>
+       </>
+     );
+}
+ 
+export default HomePage;
