2e1e38437246a50643b481917029e51d185225d6
Mahdi <khajavim497@gmail.com>
2024-07-23 18:47:08 +0430

    .

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,14 +3,14 @@ import { Button } from "./ui/button";
 
 const HomePage = () => {
   return (
-    <div className="bg-[url('src/assets/home-bg.webp')] bg-no-repeat h-full bg-cover relative overflow-hidden ">
+    <div className="flex bg-[url('src/assets/home-bg.webp')] bg-no-repeat h-full sm:bg-cover bg-center relative overflow-hidden ">
       <Navbar />
-      <div className="absolute top-[34%] left-[3%]">
-        <h2 className="italic text-4xl font-mono font-bold text-left text-pink-700">
+      <div className="absolute sm:top-[34%] sm:left-[3%] left-10">
+        <h2 className="hidden sm:block italic sm:text-4xl text-3xl w-1/2 sm:w-full font-mono font-bold text-left text-pink-700">
         Be Kind To Your Beauty
       </h2>
       </div>
-      <div className="absolute top-[43%] left-[11%]">
+      <div className="hidden sm:block absolute sm:top-[43%] top-[48%] left-[11%]">
         <Button>Explore</Button>
       </div>
       
