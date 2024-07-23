c62da4b64bf6ccceacb1ae034df8df33b9f7208b
Mahdi <khajavim497@gmail.com>
2024-07-23 18:46:47 +0430

    add hambergue menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,58 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
 
-
 const Navbar = () => {
+  const [isMenuOpen, setISMenuOpen] = useState(false);
+
   return (
     <>
-      <div>
+      <div className="hidden sm:block w-full">
         <nav>
           <ul className="flex flex-row pl-14 items-center gap-8 pt-4 text-xl font-semibold">
             <li>Home</li>
             <li>Shop</li>
             <li>Blog</li>
             <li>Contact us</li>
-            <Button className="absolute top-3 left-[31%]">Login</Button>
+            <div>
+              <Button>Login</Button>
+            </div>
           </ul>
         </nav>
+      </div>
+
+      {/* //mobile menu */}
+      
+      <div className="sm:hidden w-full h-dvh">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-8 cursor-pointer m-2"
+          onClick={() => {
+            setISMenuOpen(!isMenuOpen);
+          }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        {isMenuOpen &&
+         <div className="bg-amber-200 h-dvh rounded-lg w-1/3">
+           <ul className="flex flex-col items-center font-semibold pt-2">
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Blog</li>
+            <li>Contact us</li>
+            <div>
+              <Button>Login</Button>
+            </div>
+          </ul>
+         </div>
+         }
       </div>
     </>
   );
