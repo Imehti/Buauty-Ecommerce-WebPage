8087a686755e172af7c65c97d2eb239db976057e
Mahdi <khajavim497@gmail.com>
2024-07-25 16:02:20 +0430

    create and design product cards

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,21 +1,20 @@
+import useProducts from "@/hooks/useProducts";
 import ProductsSwiper from "./Swiper";
-
+import CardSkeleton from "./Card-Skeleton";
 
 const ProductsPage = () => {
-    return ( 
-        <>
-      <div className="h-fit">
-      <div className="flex justify-center items-center mt-12">
-            <h1 className="text-3xl font-bold">
-                Beauty Products
-            </h1>
-          
-        </div>
-        <ProductsSwiper />
-      </div>
 
-        </>
-     );
-}
- 
+  const {isLoading} =useProducts()
+  return (
+    <>
+      <div className="h-fit">
+        <div className="flex justify-center items-center mt-12">
+          <h1 className="text-3xl font-bold">Beauty Products</h1>
+        </div>
+        {isLoading ?<CardSkeleton />: <ProductsSwiper />}
+      </div>
+    </>
+  );
+};
+
 export default ProductsPage;
