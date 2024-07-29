0c8d41f6458009ad420ca22c7b2a894ec8d56a9f
Mahdi <khajavim497@gmail.com>
2024-07-29 14:42:41 +0430

    make a simple database for storing team members array

---
+++ src/services/team-members.ts
@@ -0,0 +1,42 @@
+export const TeamMembers = [
+  {
+    id: 1,
+    name: "Danielle Welling",
+    picture: "src/assets/team-members/team-avatar1.jpg",
+    expertise: "Nail master",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  },
+  {
+    id: 2,
+    name: "Danielle Welling",
+    picture: "src/assets/team-members/team-avatar2.webp",
+    expertise: "Administrator",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+},
+  {
+    id: 3,
+    name: "Danielle Welling",
+    picture: "src/assets/team-members/team-avatar3.jpg",
+    expertise: "Hair stylish",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+},
+  {
+    id: 4,
+    name: "Danielle Welling",
+    picture: "src/assets/team-members/team-avatar4.jpg",
+    expertise: "Nail master",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+},
+  {
+    id: 5,
+    name: "Danielle Welling",
+    picture: "src/assets/team-members/team-avatar5.jpg",
+    expertise: "Hair stylish",
+    description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+},
+];
