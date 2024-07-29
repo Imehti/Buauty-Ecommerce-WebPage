e06bd81891a0b048d60e9e38672462d67d170238
Mahdi <khajavim497@gmail.com>
2024-07-29 14:45:48 +0430

    create it to use team member cards in swiper

---
+++ src/components/Team-Swiper.tsx
@@ -0,0 +1,44 @@
+import { TeamMembers } from "@/services/team-members";
+import ReusableSwiper from "./Swiper";
+import TeamMembersCard from "./Team-Card";
+
+const TeamMembersSwiper = () => {
+  return (
+    <ReusableSwiper
+      data={TeamMembers}
+      renderSlide={(member) => (
+        <TeamMembersCard
+          id={member.id}
+          name={member.name}
+          picture={member.picture}
+          expertise={member.expertise}
+          description={member.description}
+        />
+      )}
+      swiperConfig={{
+     
+        breakpoints:{
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              480: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+              },
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+              },
+        }
+      }}
+      
+    />
+  );
+};
+
+export default TeamMembersSwiper;
