92e0839d420fdb7d0e7a7cf513efbe958234d41f
Mahdi <khajavim497@gmail.com>
2024-08-03 19:58:33 +0430

    add a container

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -2,13 +2,13 @@ import TeamMembersSwiper from "./Team-Swiper";
 
 const TeamSection = () => {
   return (
-    <>
+    <div className="border border-b-2">
       <div className="h-fit flex justify-center font-semibold font-serif mt-7">
         <h1 className="text-3xl">Professional Team</h1>
       </div>
 
       <TeamMembersSwiper />
-    </>
+    </div>
   );
 };
 
