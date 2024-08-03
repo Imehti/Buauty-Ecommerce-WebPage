668419ab680e8e8d006b38a1ad846500003ecb4f
Mahdi <khajavim497@gmail.com>
2024-08-03 19:57:16 +0430

    use product card skeleton

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,25 +1,33 @@
 import useProducts from "@/hooks/useProducts";
 
-import CardSkeleton from "./Card-Skeleton";
+import CardSkeleton from "./OpinionSkeleton";
 import { Button } from "./ui/button";
 import ProductsSwiper from "./Product-Swiper";
+import CardSkelton from "./CardSkelteon";
+import ProductCardSkeleton from "./CardSkelteon";
 
 const ProductsPage = () => {
+  const { isLoading, isError, error } = useProducts();
 
-  const {isLoading,isError,error} =useProducts()
-
-  if(isError) return <h1 className="text-2xl m-8 font-semibold text-destructive">{error.message}</h1>
+  if (isError)
+    return (
+      <h1 className="text-2xl m-8 font-semibold text-destructive">
+        {error.message}
+      </h1>
+    );
   return (
     <>
       <div className="h-fit">
         <div className="flex justify-center items-center mt-12">
           <h1 className="text-3xl font-bold">Beauty Products</h1>
         </div>
-        {isLoading ?<CardSkeleton />: <ProductsSwiper />}
+        {isLoading ? <ProductCardSkeleton /> : <ProductsSwiper />}
       </div>
-     <div className="flex justify-center items-center h-fit">
-     <Button className="text-xl mb-3" variant={'link'}>View All Products</Button>
-     </div>
+      <div className="flex justify-center items-center h-fit">
+        <Button className="text-xl mb-3" variant={"link"}>
+          View All Products
+        </Button>
+      </div>
     </>
   );
 };
