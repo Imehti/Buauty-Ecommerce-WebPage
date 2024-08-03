71eb0b432156d80b14d315c9a259314b9a0b7a79
Mahdi <khajavim497@gmail.com>
2024-08-03 18:47:20 +0430

    add margin top

--- src/components/Card-Skeleton.tsx
+++ src/components/Card-Skeleton.tsx
@@ -1,7 +1,7 @@
 const CardSkeleton = () => {
   return (
     <>
-      <div className="bg-white p-2 sm:p-4 sm:h-64 rounded-2xl shadow-lg flex flex-col sm:flex-row gap-5 select-none ">
+      <div className="bg-white lg:mt-12 mt-8 p-2 sm:p-4 sm:h-64 rounded-2xl shadow-lg flex flex-col sm:flex-row gap-5 select-none ">
         <div className="h-52 sm:h-full sm:w-72 rounded-xl bg-gray-200 animate-pulse"></div>
         <div className="flex flex-col flex-1 gap-5 sm:p-2">
           <div className="flex flex-1 flex-col gap-3">
