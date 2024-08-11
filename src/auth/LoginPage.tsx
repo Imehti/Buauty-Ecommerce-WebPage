d8d2e40d1e30b56987093986bd4d62925aade87c
Mahdi <khajavim497@gmail.com>
2024-08-11 18:33:28 +0430

    craete login page file and set background

--- src/auth/LoginPage.tsx
+++ src/auth/LoginPage.tsx
@@ -1,9 +1,17 @@
+
 const LoginPage = () => {
-    return ( 
-        <>
-        
-        </>
-     );
-}
- 
+  return (
+    <>
+    
+      <div className="grid grid-cols-3">
+        <div className="loginPageBg col-span-2">
+        </div>
+          <div>
+
+          </div>
+      </div>
+    </>
+  );
+};
+
 export default LoginPage;
