517f9db4bca3647205128fb821ce61ea33f2046b
Mahdi <khajavim497@gmail.com>
2024-08-11 22:46:34 +0430

    create a reusable login form

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface LoginFormProps {
+  title:string,
+  
+}
+
+const LoginForm = () => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default LoginForm;
