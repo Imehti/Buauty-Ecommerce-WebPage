bb9b691dacacd416baa9c43dd0716cddd94724b1
Mahdi <khajavim497@gmail.com>
2024-08-12 22:22:10 +0430

    add register page url to our routes

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,10 +1,12 @@
 import App from "@/App";
-import LoginPage from "@/components/login/LoginPage";
+import LoginPage from "@/components/auth/LoginPage";
+import RegisterPage from "@/components/auth/RegisterPage";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPage /> },
 ]);
 
 export default router;
