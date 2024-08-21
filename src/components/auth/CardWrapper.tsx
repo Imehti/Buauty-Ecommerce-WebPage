bf1157336cad52784d5963e38f5653dec15d2eb2
Mahdi <khajavim497@gmail.com>
2024-08-21 19:03:56 +0430

    cerate reusable card wrapper using in login and register form

---
+++ src/components/auth/CardWrapper.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import Header from "./Header";
+import BackButton from "./BackButton";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLable: string;
+  backButtonHref: string;
+  backButtonLable: string;
+  showSocial?: boolean;
+}
+
+const CardWrapper = ({
+  children,
+  headerLable,
+  backButtonHref,
+  backButtonLable,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <Header lable={headerLable} />
+        </CardHeader>
+        <CardContent>{children}</CardContent>
+        <CardFooter>
+        <BackButton lable={backButtonLable} href={backButtonHref} />
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default CardWrapper;
