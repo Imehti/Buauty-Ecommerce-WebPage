b1dbbd76c652fd41c4a474a160413d3320d4dd55
Mahdi <khajavim497@gmail.com>
2024-08-21 18:58:39 +0430

    create login and register schema using zod

---
+++ src/schemas/index.ts
@@ -0,0 +1,28 @@
+import * as z from "zod"
+
+const passwordValidation = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+  );
+
+export const LoginSchema=z.object({
+    email:z.string().email({
+        message:"Email is required"
+    }),
+    password:z.string().min(1,{
+        message:"Password is required"
+    })
+})
+
+export const RegisterSchema= z.object({
+    email:z.string().email({
+        message:"Email is required"
+    }),
+    password:z.string().min(1,{
+        message:"Minimum password 6 characters"
+    }) .regex(passwordValidation, {
+        message: 'Your password is not valid',
+      }),
+    name:z.string().min(1,{
+        message:"Name is required"
+    })
+})
