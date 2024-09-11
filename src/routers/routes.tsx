d2cef8aad02f1aa1aab88370b4d515215a91e467
Mahdi <khajavim497@gmail.com>
2024-09-11 17:34:44 +0430

    make protected routes

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,10 +1,27 @@
 import App from "@/App";
+import Cart from "@/components/Cart";
 import LoginPage from "@/components/auth/LoginPage";
 import RegisterPage from "@/components/auth/RegisterPage";
-import { createBrowserRouter } from "react-router-dom";
+import currentUserState from "@/selector/currentUser";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+
+const RequireAuth=({children}:any) => {
+ const currentUser=useRecoilValue(currentUserState)
+
+  return currentUser  ? children : <Navigate to="/login" />
+}
+
+
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
+  { path: "/", element: <App />},
+  { path: "/Cart",   element: (
+    <RequireAuth>
+      <Cart />
+    </RequireAuth>
+  ),},
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
 ]);
