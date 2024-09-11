812381ba85c050751cc0dd898d67feb5a37cad34
Mahdi <khajavim497@gmail.com>
2024-09-11 17:31:58 +0430

    render app in recoil root

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/routes.tsx";
+import {
+  RecoilRoot,
+} from 'recoil';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+  <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
