4fb4f0c26cd86010c5913ee3288ce85113b2a2f4
Mahdi Khajavi <khajavim497@gmail.com>
2024-11-28 11:04:49 +0330

    refetch data when this component is mounted

--- src/components/Product-Swiper.tsx
+++ src/components/Product-Swiper.tsx
@@ -1,15 +1,22 @@
 import useProducts from "@/hooks/useProducts";
 import ProductCards from "./Product-cards";
 import ReusableSwiper from "./Swiper";
+import { useEffect } from "react";
 
 const ProductsSwiper = () => {
-  const { data: products } = useProducts();
+  const { data , refetch } = useProducts();
 
+  useEffect(()=>{
+    refetch()
+  },[])
+
+ const products=data?.slice(0,30)
   return (
     <ReusableSwiper
       data={products}
       renderSlide={(product) => (
         <ProductCards
+        //id={product.id}
           name={product.name}
           product_type={product.product_type}
           price={product.price}
