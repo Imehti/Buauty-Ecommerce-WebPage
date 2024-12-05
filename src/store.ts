8a3ca6ae3dabed8d3363e3e8c69c581aae387671
Mahdi Khajavi <khajavim497@gmail.com>
2024-12-05 16:04:19 +0330

    Add cart reducer to Redux store configuration

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./features/counter-slice";
+import { cartReducer } from "./features/cart-slice";
 const store = configureStore({
   reducer: {
-    counter:counterReducer
+    counter:counterReducer,
+    cart:cartReducer
   },
 });
 
