c3dbd1c080a8f85de139b425c733fd2374814459
Mahdi Khajavi <khajavim497@gmail.com>
2024-12-09 12:09:51 +0330

    make it responsive

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -9,7 +9,6 @@ import { useAppDispatch, useAppSelector } from "@/hooks/typedhooks";
 import { addToCart } from "@/features/cart-slice";
 import { useRecoilValue } from "recoil";
 import currentUserState from "@/selector/currentUser";
-
 const ProductDetailsPage = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -48,6 +47,7 @@ const ProductDetailsPage = () => {
       navigate("/login");
     }
   };
+
   const incrementLocalQuantity = () => {
     setLocalQuantity((prev) => prev + 1);
   };
@@ -63,16 +63,16 @@ const ProductDetailsPage = () => {
 
   return (
     <>
-      <div className="grid grid-cols-3 grid-rows-1 p-10">
-        <div className="col-span-2 ml-12 p-12">
+      <div className="grid grid-cols-1 lg:grid-cols-3 p-10 h-fit md:relative">
+        <div className="md:absolute col-span-1 lg:col-span-2 ml-0 lg:ml-12 p-4 lg:p-12 top-0">
           <img
-            className="w-[70%] h-[70%] object-fill pt-8"
+            className="w-full md:w-[60%] h-auto object-fill pt-8"
             src={product?.api_featured_image}
             alt={product?.name}
           />
-          <div className="h-fit mt-12 grid grid-cols-5 gap-5">
-            {product?.product_colors.slice(0,20).map((item) => (
-              <div className="flex space-x-3">
+          <div className="h-fit mt-12 grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-5 gap-5">
+            {product?.product_colors.slice(0, 20).map((item) => (
+              <div className="flex space-x-3" key={item.colour_name}>
                 <div
                   className={`w-6 h-6 rounded-full`}
                   style={{ backgroundColor: item.hex_value }}
@@ -82,10 +82,10 @@ const ProductDetailsPage = () => {
             ))}
           </div>
         </div>
-        <div className="flex flex-col items-start justify-start h-2/3 m-6 space-y-3">
+        <div className="md:absolute flex flex-col items-start justify-start h-fit m-4 lg:m-6 space-y-8 left-[60%] top-[80%]">
           {/* Product Name */}
-          <h1 className="text-2xl font-bold">{product?.name}</h1>
-          <h1 className="text-xl font-bold">{product?.product_type}</h1>
+          <h1 className="text-xl lg:text-2xl font-bold">{product?.name}</h1>
+          <h1 className="text-lg lg:text-xl font-bold">{product?.product_type}</h1>
 
           {/* Price */}
           <span className="text-lg text-gray-700">
@@ -107,7 +107,7 @@ const ProductDetailsPage = () => {
           </p>
 
           {/* Quantity and Add to Cart */}
-          <div className="flex items-center space-x-3">
+          <div className="flex flex-col sm:flex-row items-center space-x-0 sm:space-x-3 p-4">
             {!cartItem ? (
               // If not in cart, use local quantity logic
               <div className="flex items-center border border-gray-300 rounded-md overflow-hidden w-44 h-fit">
@@ -138,12 +138,12 @@ const ProductDetailsPage = () => {
             {!cartItem ? (
               <Button
                 onClick={handleAddToCart}
-                className="bg-green-600 text-white px-4 py-2"
+                className="bg-green-600 text-white px-4 py-2 mt-4 sm:mt-0"
               >
                 Add To Cart
               </Button>
             ) : (
-              <span className="text-green-600 font-semibold mt-8">
+              <span className="text-green-600 font-semibold mt-4 sm:mt-0">
                 <Link to={"/cart"}>Already in Cart</Link>
               </span>
             )}
