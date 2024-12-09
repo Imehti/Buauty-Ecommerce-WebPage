26a824f433a5ecac94ce2c6d0bb5154dd8e35baf
Mahdi Khajavi <khajavim497@gmail.com>
2024-12-09 15:54:18 +0330

    make it responsive

--- src/components/auth/RegisterPage.tsx
+++ src/components/auth/RegisterPage.tsx
@@ -1,18 +1,18 @@
 import RegisterForm from "./RegisterForm";
 
-const RegisterPage = () => {
+const LoginPage = () => {
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="loginPageBg"></div>
-        <div className="LoginForm">
-            <div className="h-fit flex justify-center items-center mt-[20%]">
-       <RegisterForm />
-        </div>
+      <div className="grid grid-cols-3 md:grid-cols-2">
+        <div className="hidden loginPageBg md:block"></div>
+        <div className="LoginForm col-span-3 md:col-span-1">
+          <div className="h-fit flex justify-center items-center mt-[20%]">
+            <RegisterForm />
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
