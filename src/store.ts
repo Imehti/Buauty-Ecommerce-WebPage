88828cadca54321df4453c1fb56b0bf695bd0c71
Mahdi Khajavi <khajavim497@gmail.com>
2024-12-09 16:03:15 +0330

    add filter reducer

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./features/counter-slice";
 import { cartReducer } from "./features/cart-slice";
+import { filterReducer } from "./features/filter-slice";
 const store = configureStore({
   reducer: {
     counter:counterReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    filters:filterReducer
   },
 });
 
