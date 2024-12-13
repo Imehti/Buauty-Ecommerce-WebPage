f64370cd9383799a79e745a278dc49168f95b91f
Mahdi Khajavi <khajavim497@gmail.com>
2024-12-13 16:12:44 +0330

    fix any type error

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,13 +2,14 @@ import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase";
+import { FirebaseError } from "firebase/app";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) return { error: "invalid fields" };
 
-  const { password, name, email } = validatedFields.data;
+  const { password, email } = validatedFields.data;
 
   try {
     const userCredential = await createUserWithEmailAndPassword(
@@ -19,9 +20,10 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const user = userCredential.user;
     console.log(user);
     return { user, success: "User created successfully", error: null };
-  } catch (error: any) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    return { error: errorMessage };
+  } catch (error: unknown) {
+    if (error instanceof FirebaseError) {
+      return { error: error.message };
+    }
+    return { error: "An unexpected error occurred" };
   }
 };
